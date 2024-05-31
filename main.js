be351199f14acb5f488e44d0e809a53274350299
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2024-05-31 18:46:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+"use strict";
+//              🚀 Day 11 Challenge: Start Coding! 🚀
+Object.defineProperty(exports, "__esModule", { value: true });
+//Question 31
+//No Users: Ensure your user list isn’t empty.
+let usernames = [];
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    console.log("Hello usernames, thank you for logging in again");
+}
+//Question 32:
+//Checking Usernames: Ensure uniqueness in usernames.
+let current_users = ["user1", "admin", "user3", "user4", "user5"];
+let new_users = ["User1", "User6", "user7", "admin", "User9"];
+new_users.forEach(newUser => {
+    if (current_users.some(currentUser => currentUser.toLowerCase() === newUser.toLowerCase())) {
+        console.log(`${newUser} will need to enter a new username.`);
+    }
+    else {
+        console.log(`${newUser} is available.`);
+    }
+});
+//Question 33: 
+//Ordinal Numbers: Display numbers with their ordinal suffixes.
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+numbers.forEach((number) => {
+    let suffix = "th";
+    if (number === 1) {
+        suffix = "st";
+    }
+    else if (number === 2) {
+        suffix = "nd";
+    }
+    else if (number === 3) {
+        suffix = "rd";
+    }
+    console.log(`${number}${suffix}`);
+});
